f853a76c0fa5057380a7b3eb9c9ef6c88682e180
Leandro Cavalcanti <leandrocavalcanti499@gmail.com>
2023-08-30 12:34:34 -0300

    First commit

---
+++ src/main/java/com/lcdev/restrimais/RestrimaisApplication.java
@@ -0,0 +1,13 @@
+package com.lcdev.restrimais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestrimaisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestrimaisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lcdev/restrimais/RestrimaisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lcdev.restrimais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestrimaisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
